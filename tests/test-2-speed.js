17792f90abe78856840b468f4a319f8d99e41ca5
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2022-06-20 15:27:29 +0300

    Update speed test

--- tests/test-2-speed.js
+++ tests/test-2-speed.js
@@ -1,13 +1,96 @@
 import {decode, encode} from "../index.js";
 
-const charset = "ascii85";
-const long = "a".repeat(1_000_002*5);
+const input = "QWERTY1234".repeat(100_000*5);
+console.log(`Input: ${input.length} chars string`);
+
+// -----------------------------------
+console.log("\n--- AlttiRi/base85 ---");
+
+console.time("total");
+console.time("encode");
+const encodedAb = new TextEncoder().encode(input);
+const encoded1 = encode(encodedAb);
+console.timeEnd("encode");
+
+console.time("decode");
+const decodedAB = decode(encoded1);
+const output1 = new TextDecoder().decode(decodedAB);
+console.timeEnd("decode");
+console.timeEnd("total");
+
+console.log("input === output1:", input === output1);
+
+/*
+Input: 5000000 chars string
+
+--- AlttiRi/base85 ---
+encode: 51.036ms
+decode: 28.376ms
+total: 79.884ms
+input === output1: true
+
+--- noseglid/base85 ---
+encode: 743.496ms
+decode: 80.996ms
+total: 825.063ms
+input   === output2: true
+output1 === output2: true
+
+--- Sheep-y/Base85 ---
+encode: 70.319ms
+decode: 40.662ms
+total: 111.481ms
+input   === output3: true
+output1 === output3: true
+ */
 
 
-console.time(`decode -> encode ${long.length} chars`);
-const longAb = new TextEncoder().encode(long);
-const longEnc = encode(longAb, charset);
-const longDec = decode(longEnc, charset);
-console.timeEnd(`decode -> encode ${long.length} chars`);
+// Uncomment and install "base85"
+/*
+// -----------------------------------
+console.log("\n--- noseglid/base85 ---");
+// https://github.com/noseglid/base85
 
-console.log(`decode -> encode ${long.length} chars:`, longAb.toString() === longDec.toString());
+
+import base85 from "base85";
+console.time("total");
+console.time("encode");
+const encoded2 =  base85.encode(input).toString();
+console.timeEnd("encode");
+
+console.time("decode");
+const output2 = base85.decode(encoded2).toString();
+console.timeEnd("decode");
+console.timeEnd("total");
+
+console.log("input   === output2:", input === output2);
+console.log("output1 === output2:", output1 === output2);
+*/
+
+
+// Uncomment and copy-paste the code from GitHub
+/*
+// -----------------------------------
+console.log("\n--- Sheep-y/Base85 ---");
+
+// put code here from
+// https://github.com/Sheep-y/Base85/blob/master/javascript/base85.js
+(function() {})(
+    ...
+);
+
+
+console.time("total");
+console.time("encode");
+const encoded3 = Base85.Z85Encoder.encode(input);
+console.timeEnd("encode");
+
+console.time("decode");
+const output3 = Base85.Z85Decoder.decode(encoded3);
+console.timeEnd("decode");
+console.timeEnd("total");
+
+console.log("input   === output3:", input === output3);
+console.log("output1 === output3:", output1 === output3);
+
+*/
